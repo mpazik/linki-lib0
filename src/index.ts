a288a4811ea9b139290aebd6df2f336e27ace29c
Marek Pazik <contact@marekpazik.pl>
2022-04-27 10:54:29 +1000

    Functions refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import * as array from "./array";
-import * as functions from "./functions";
+import * as functions from "./function";
 import * as indexeddb from "./indexeddb";
 import * as iterator from "./iterator";
+import * as object from "./object";
 import * as set from "./set";
 
-export { array, functions, iterator, set, indexeddb };
+export { array, functions, iterator, set, indexeddb, object };
--- src/set.ts
+++ src/set.ts
@@ -1,7 +1,7 @@
 import { filter } from "./iterator";
 
 export const setDifference = <T>(setA: Set<T>, setB: Set<T>): Set<T> =>
-  new Set(filter(setA, (x) => !setB.has(x)));
+  new Set(filter(setA, (x: T) => !setB.has(x)));
 
 export const setIntersection = <T>(setA: Set<T>, setB: Set<T>): Set<T> =>
-  new Set(filter(setA, (x) => setB.has(x)));
+  new Set(filter(setA, (x: T) => setB.has(x)));
